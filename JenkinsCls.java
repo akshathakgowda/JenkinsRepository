accc46c66b103f74ad96494d734f477414473ff3
akshathakgowda <38320265+akshathakgowda@users.noreply.github.com>
2018-04-23 13:19:50 +0530

    Add files via upload

---
+++ JenkinsCls.java
@@ -0,0 +1,12 @@
+package maven.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class JenkinsCls {
+
+	@Test
+	public void jenkins()
+	{
+		System.out.println("Jenkins is fully up and running");
+	}
+}
